04771ebf6eae581410efb35251afd5338c4af908
Davide Todorov <davide.todorov@abv.bg>
2020-08-19 10:45:25 +0300

    Update Calculator.java

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,2 +1,20 @@
+import javax.swing.*;
+
 public class Calculator {
+
+    private JFrame frame = new JFrame("Calculator");
+
+    public Calculator(){
+        //Frame attributes
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(450,600);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setLayout(null);
+
+    }
+
+    public static void main(String[] args) {
+        new Calculator();
+    }
 }
