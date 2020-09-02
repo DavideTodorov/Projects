4695b0019e33385555c918840ee5119630e5b8b1
Davide Todorov <davide.todorov@abv.bg>
2020-09-02 17:15:52 +0300

    Update TicTacToe.java
    Declared all variables needed

--- Tic-Tac-Toe/src/TicTacToe.java
+++ Tic-Tac-Toe/src/TicTacToe.java
@@ -1,2 +1,52 @@
-public class TicTacToe {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class TicTacToe implements ActionListener {
+
+    Random random = new Random();
+    JFrame frame = new JFrame();
+    JPanel textPanel = new JPanel();
+    JPanel buttonPanel = new JPanel();
+    JLabel textField = new JLabel();
+    JButton[] buttons = new JButton[9];
+    boolean firstPlayerTurn;
+
+
+    //Constructor
+    TicTacToe() {
+
+    }
+
+
+    //creates the first move
+    public void firstTurn(){
+
+    }
+
+
+    //checks if there are win conditions
+    public void checkForWin(){
+
+    }
+
+
+    //method to execute if X wins
+    public void xWins(int a, int b, int c){
+
+    }
+
+
+    //method to execute if O wins
+    public void oWins(int a, int b, int c){
+
+    }
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
