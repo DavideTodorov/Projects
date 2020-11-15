76d71a7cfdab434baf3547283431f2448ed25dd0
Davide Todorov <davide.todorov@abv.bg>
2020-11-15 20:13:43 +0200

    Create UserAccountClass

--- ConsoleProjects/LoggingApp/src/Main.java
+++ ConsoleProjects/LoggingApp/src/Main.java
@@ -5,6 +5,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        System.out.println("Register user:");
+
+        while (true) {
+            System.out.println("Enter name:");
+            String name = scanner.nextLine();
+
+            System.out.println("Enter email:");
+            String email = scanner.nextLine();
+
+            System.out.println("Enter password:");
+            String password = scanner.nextLine();
+
+            try {
+                UserAccount userAccount = new UserAccount
+                        (name, email, password);
+                System.out.printf("User %s created!%n", name);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Try again!");
+            }
+        }
     }
 }
---
+++ ConsoleProjects/LoggingApp/src/UserAccount.java
@@ -0,0 +1,48 @@
+public class UserAccount {
+
+    private String userName;
+    private String email;
+    private String password;
+
+    public UserAccount(String userName, String email, String password) {
+        this.setUsername(userName);
+        this.setEmail(email);
+        this.setPassword(password);
+    }
+
+    private void setPassword(String password) {
+        if (password.length() <= 4) {
+            throw new IllegalArgumentException(
+                    "Password should be at least 5 characters");
+        }
+
+        this.password = password;
+    }
+
+    private void setEmail(String email) {
+        if (!email.contains("@")) {
+            throw new IllegalArgumentException(
+                    "Email should contain '@'");
+        }
+
+        this.email = email;
+    }
+
+    private void setUsername(String userName) {
+        if (userName.length() <= 4 || userName.length() > 15) {
+            throw new IllegalArgumentException(
+                    "UserName should be between 4 and 15 characters");
+        }
+
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Username: %s%n" +
+                        "Email: %s%n" +
+                        "Password: %s", this.userName,
+                this.email,
+                this.password.replaceAll("\\w", "*"));
+    }
+}
