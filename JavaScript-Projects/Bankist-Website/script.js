0c5b5e4c5f8e058a775a137f7f336f8ca7ca16e0
Davide Todorov <davide.todorov@abv.bg>
2021-01-19 15:07:10 +0200

    Scroll smoothly when press "Learn more" button

--- JavaScript-Projects/Bankist-Website/script.js
+++ JavaScript-Projects/Bankist-Website/script.js
@@ -2,12 +2,13 @@
 
 ///////////////////////////////////////
 // Modal window
-
+//Elements
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 
+//Functionality
 const openModal = function (e) {
   e.preventDefault();
   modal.classList.remove("hidden");
@@ -19,13 +20,24 @@ const closeModal = function () {
   overlay.classList.add("hidden");
 };
 
+//Buttons event listeners
 btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
-
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
 
+//Close when "ESC" is pressed
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
   }
+});
+
+/////////////////////////////////////////////////
+//Scroll smoothly when press "Learn more" button
+
+const btnLearnMore = document.querySelector(".btn--scroll-to");
+const section1 = document.getElementById("section--1");
+
+btnLearnMore.addEventListener("click", function (e) {
+  section1.scrollIntoView({ behavior: "smooth" });
 });
