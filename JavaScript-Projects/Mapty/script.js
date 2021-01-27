a70924f052dd0facbabd3e6458f43af8505964f9
Davide Todorov <davide.todorov@abv.bg>
2021-01-27 07:42:06 +0200

    Add workouts to the list of workouts

--- JavaScript-Projects/Mapty/script.js
+++ JavaScript-Projects/Mapty/script.js
@@ -1,7 +1,19 @@
 "use strict";
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 //Elements
 const form = document.querySelector(".form");
@@ -23,14 +35,23 @@ class Workout {
     this.distance = distance;
     this.duration = duration;
   }
+
+  _createDescription() {
+    this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
+      months[this.date.getMonth()]
+    } ${this.date.getDate()}`;
+  }
 }
 
 //Child classes of the Workout class
 class Running extends Workout {
+  type = "running";
+
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
     this.cadence = cadence;
     this._calcPace();
+    this._createDescription();
   }
 
   _calcPace() {
@@ -41,10 +62,13 @@ class Running extends Workout {
 }
 
 class Cycling extends Workout {
+  type = "cycling";
+
   constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
     this._calcSpeed();
+    this._createDescription();
   }
 
   _calcSpeed() {
@@ -169,6 +193,9 @@ class App {
     //Add new object to workouts array
     this.#workouts.push(workout);
 
+    //Render workout on the workout list
+    this._addWorkoutToTheList(workout);
+
     //Render workout on the map as a marker
     this._renderWorkoutMarker(workout);
 
@@ -186,11 +213,64 @@ class App {
           minWidth: 100,
           autoClose: false,
           closeOnClick: false,
-          className: `${workout.constructor.name.toLowerCase()}-popup`,
+          className: `${workout.type}-popup`,
         })
       )
-      .setPopupContent("You are here")
+      .setPopupContent(
+        `${workout.type === "running" ? "🏃‍♂️" : "🚴‍♀️"} ${workout.description}`
+      )
       .openPopup();
+  }
+
+  _addWorkoutToTheList(workout) {
+    let elementHtml = `
+    <li class="workout workout--${workout.type}" data-id=${workout.id}>
+      <h2 class="workout__title">${workout.description}</h2>
+      <div class="workout__details">
+        <span class="workout__icon">${
+          workout.type === "running" ? "🏃‍♂️" : "🚴‍♀️"
+        }</span>
+        <span class="workout__value">${workout.distance}</span>
+        <span class="workout__unit">km</span>
+      </div>
+      <div class="workout__details">
+        <span class="workout__icon">⏱</span>
+        <span class="workout__value">${workout.duration}</span>
+        <span class="workout__unit">min</span>
+      </div>
+    `;
+
+    if (workout.type === "running") {
+      elementHtml += `
+          <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.pace.toFixed(1)}</span>
+            <span class="workout__unit">min/km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">🦶🏼</span>
+            <span class="workout__value">${workout.cadence}</span>
+            <span class="workout__unit">spm</span>
+          </div>
+        </li>
+      `;
+    } else {
+      elementHtml += `
+          <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${workout.speed.toFixed(1)}</span>
+            <span class="workout__unit">km/h</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⛰</span>
+            <span class="workout__value">${workout.elevationGain}</span>
+            <span class="workout__unit">m</span>
+          </div>
+        </li>
+      `;
+    }
+
+    form.insertAdjacentHTML("afterend", elementHtml);
   }
 
   //Method to reset all input fields
@@ -199,7 +279,17 @@ class App {
       "";
     inputElevation.blur();
     inputCadence.blur();
+    //Hide form
+    this._hideForm();
+  }
+
+  //Method to hide the form
+  _hideForm() {
+    form.style.display = "none";
     form.classList.add("hidden");
+    setTimeout(function () {
+      form.style.display = "grid";
+    }, 1000);
   }
 }
 
