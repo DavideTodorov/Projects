9444332fda88194b48a45e696526d538a53bd9c0
Davide Todorov <davide.todorov@abv.bg>
2021-03-02 07:17:27 +0200

    Make some changes

--- Java-Projects/FlappyBird/core/src/com/flappybird/game/states/MenuState.java
+++ Java-Projects/FlappyBird/core/src/com/flappybird/game/states/MenuState.java
@@ -11,6 +11,8 @@ public class MenuState extends State {
 
     public MenuState(GameStateManager gameStateManager) {
         super(gameStateManager);
+        this.camera.setToOrtho(false, FlappyBird.WIDTH / 2,
+                FlappyBird.HEIGHT / 2);
         this.background = new Texture("bg.png");
         this.playButton = new Texture("playbtn.png");
     }
@@ -29,10 +31,11 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch spriteBatch) {
+        spriteBatch.setProjectionMatrix(camera.combined);
         spriteBatch.begin();
-        spriteBatch.draw(background, 0, 0, FlappyBird.WIDTH, FlappyBird.HEIGHT);
-        spriteBatch.draw(playButton, (FlappyBird.WIDTH / 2) - (playButton.getWidth() / 2),
-                (FlappyBird.HEIGHT / 2));
+        spriteBatch.draw(background, 0, 0);
+        spriteBatch.draw(playButton, camera.position.x - playButton.getWidth() / 2,
+                camera.position.y);
         spriteBatch.end();
     }
 
