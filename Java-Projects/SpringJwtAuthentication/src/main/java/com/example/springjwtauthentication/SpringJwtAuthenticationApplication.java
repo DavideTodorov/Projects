76a9d7b561f58f45b2a38d45f5709ada142cc9a7
Davide Todorov <davidetodorov02@gmail.com>
2023-09-21 09:35:21 +0300

    Registering JWT token

--- Java-Projects/SpringJwtAuthentication/src/main/java/com/example/springjwtauthentication/SpringJwtAuthenticationApplication.java
+++ Java-Projects/SpringJwtAuthentication/src/main/java/com/example/springjwtauthentication/SpringJwtAuthenticationApplication.java
@@ -1,9 +1,12 @@
 package com.example.springjwtauthentication;
 
+import com.example.springjwtauthentication.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class SpringJwtAuthenticationApplication {
 
     public static void main(String[] args) {
